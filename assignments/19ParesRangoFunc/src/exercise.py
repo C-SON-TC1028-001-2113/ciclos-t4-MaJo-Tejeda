ee56b8200ba77485e95ba0fc912595d4a34f4aec
MaJo-Tejeda <88874353+MaJo-Tejeda@users.noreply.github.com>
2021-09-06 17:54:13 -0700

    Update exercise.py

--- assignments/19ParesRangoFunc/src/exercise.py
+++ assignments/19ParesRangoFunc/src/exercise.py
@@ -1,6 +1,13 @@
 
 def main():
 
+v1 = int(input('Enter value 1: '))
+v2 = int(input('Enter value 2: '))
 
+for x in range (v1,v2):
+    if x % 2 == 0:
+        print (x)
+
+    
 if __name__=='__main__':
     main()
