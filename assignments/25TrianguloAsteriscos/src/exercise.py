1290f3b18a1e731a9c21ff6897e90ae7e4dd07fb
MaJo-Tejeda <88874353+MaJo-Tejeda@users.noreply.github.com>
2021-09-06 17:23:36 -0700

    Update exercise.py

--- assignments/25TrianguloAsteriscos/src/exercise.py
+++ assignments/25TrianguloAsteriscos/src/exercise.py
@@ -1,6 +1,18 @@
 
 def main():
-    #escribe tu código abajo de esta línea
+
+
+height = int(input("Enter triangle height: "))
+x = 2 * height - 2
+for y in range(0, height):
+    for z in range(0, x):
+        print(end=" ")
+    x = x - 2
+    for z in range(0, y + 1):
+        print("* ", end="")
+    print("")
+
+
 
 
 if __name__=='__main__':
